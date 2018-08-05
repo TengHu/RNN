9a2cb5d71b4a232161f078b4be81b479cc287980
Niel Hu <niel.hu@uber.com>
2018-08-05 10:06:14 -0700

    update

--- generate_corpus.py
+++ generate_corpus.py
@@ -1,12 +1,33 @@
 import data
+import argparse
 
 try:
     import cPickle as pickle
 except ModuleNotFoundError:
     import pickle
 
+###############################################################################
 
-corpus = data.get_corpus(path="data/wsj_sample")
-with open("data/wsj_corpus", 'wb') as file:
+parser = argparse.ArgumentParser(description='Generate Corpus File')
+
+parser.add_argument(
+    '--input',
+    type=str,
+    default="",
+    help='input file')
+
+parser.add_argument(
+    '--output',
+    type=str,
+    default="",
+    help='output file')
+
+args = parser.parse_args()
+
+###############################################################################
+
+
+corpus = data.get_corpus(path=args.input)
+with open(args.output, 'wb') as file:
     pickle.dump(corpus, file, pickle.HIGHEST_PROTOCOL)
 
